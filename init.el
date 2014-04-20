343ab76f2b77bcdfc5e08460e91e8c521c4787f5
Junpei Anzai <junpei.anzai@gmail.com>
2014-04-20 16:39:34 +0900

    gnuplot-mode

--- init.el
+++ init.el
@@ -360,3 +360,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+ ; gnuplot-mode
+(autoload 'gnuplot-mode "gnuplot" "gnuplot major mode" t)
+(autoload 'gnuplot-make-buffer "gnuplot" "open a buffer in gnuplot mode" t)
+;; this line automatically causes all files with the .gp extension to
+;; be loaded into gnuplot mode
+(setq auto-mode-alist (append '(("\\.gp$" . gnuplot-mode)) auto-mode-alist))
+

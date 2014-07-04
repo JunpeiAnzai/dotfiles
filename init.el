35924d403eb1cd9c35ca3212f143d52e0b436771
Junpei Anzai <junpei.anzai@gmail.com>
2014-07-04 09:57:55 +0900

    updated for emacs24.3.92

--- init.el
+++ init.el
@@ -47,8 +47,8 @@
 ;; Eshell
 ;;====================
 ;;
-;;(add-hook 'after-init-hook  (lambda() (eshell)))
-;;
+(add-hook 'after-init-hook  (lambda() (eshell)))
+
 (add-hook 'after-init-hook
           (lambda()
             (eshell)
@@ -118,7 +118,7 @@
 (setq twittering-use-master-password t)
 
 (show-paren-mode 1)
-
+ 
 (menu-bar-mode -1)
 
 (tool-bar-mode -1)
@@ -142,11 +142,11 @@
 (require 'tramp)
 (setq tramp-default-method "sshx")
 
-;;aspell
-(setq-default ispell-program-name "aspell")
-;; aspell with japanese english mixture
-(eval-after-load "ispell"
-'(add-to-list 'ispell-skip-region-alist '("[^\000-\377]+")))
+;; ;;aspell
+;; (setq-default ispell-program-name "aspell")
+;; ;; aspell with japanese english mixture
+;; (eval-after-load "ispell"
+;; '(add-to-list 'ispell-skip-region-alist '("[^\000-\377]+")))
 ;; YaTeX+flyspell-mode
 ;(add-hook 'yatex-mode-hook 'flyspell-mode)
 ;(custom-set-variables 
@@ -186,11 +186,14 @@
       "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
   '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(require 'package)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/theme")
 ;(load-theme 'solarized-light t t) 
 (load-theme 'solarized-dark t t)
+(enable-theme 'solarized-dark)
 
 ; ignore Capital Case
 (setq read-file-name-completion-ignore-case t)
@@ -218,17 +221,17 @@
 ;; auto-complete f90 mode
 
 
-;;;
-;;; org-mode
-;;;
-(require 'remember)
-(require 'org-remember)
+
+;; org-mode
+
+;(require 'remember)
+;(require 'org-remember)
 ;; org-modeの初期化
 (require 'org-install)
 ;; キーバインドの設定
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
-(define-key global-map "\C-cr" 'org-remember)
+;(define-key global-map "\C-cr" 'org-remember)
 ;; 拡張子がorgのファイルを開いた時，自動的にorg-modeにする
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 ;; org-modeでの強調表示を可能にする
@@ -243,14 +246,14 @@
 (setq org-startup-truncated nil)
 (setq org-return-follows-link t)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-(org-remember-insinuate)
+;(org-remember-insinuate)
 (setq org-directory "~/Dropbox/")
 (setq org-default-notes-file (concat org-directory "agenda.org"))
-(setq org-remember-templates
-      '(("Todo" ?t "** TODO %?\n   %i\n   %a\n   %t" nil "Inbox")
-        ("Bug" ?b "** TODO %?   :bug:\n   %i\n   %a\n   %t" nil "Inbox")
-        ("Idea" ?i "** %?\n   %i\n   %a\n   %t" nil "New Ideas")
-        ))
+;; (setq org-remember-templates
+;;       '(("Todo" ?t "** TODO %?\n   %i\n   %a\n   %t" nil "Inbox")
+;;         ("Bug" ?b "** TODO %?   :bug:\n   %i\n   %a\n   %t" nil "Inbox")
+;;         ("Idea" ?i "** %?\n   %i\n   %a\n   %t" nil "New Ideas")
+;;         ))
 
 
 (setq indent-line-function 'indent-relative-maybe)
@@ -311,29 +314,29 @@
 	  '(lambda ()
 	     (flymake-mode t)))
 
-;; (add-hook 'find-file-hook 'flymake-find-file-hook)
-;;; Makefile doesn't exist
-(defun my:flymake-simple-generic-init (cmd &optional opts)
-  (let* ((temp-file (flymake-init-create-temp-buffer-copy
-		     'flymake-create-temp-inplace))
-	 (local-file (file-relative-name
-		      temp-file
-		      (file-name-directory buffer-file-name))))
-    (list cmd (append opts (list local-file)))))
-;;; Makefile exist
+;; ;; (add-hook 'find-file-hook 'flymake-find-file-hook)
+;; ;;; Makefile doesn't exist
+;; (defun my:flymake-simple-generic-init (cmd &optional opts)
+;;   (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;; 		     'flymake-create-temp-inplace))
+;; 	 (local-file (file-relative-name
+;; 		      temp-file
+;; 		      (file-name-directory buffer-file-name))))
+;;     (list cmd (append opts (list local-file)))))
+;; Makefile exist
 ;; (defun my:flymake-simple-make-or-generic-init (cmd &optional opts)
 ;;   (if (file-exists-p "Makefile")
 ;;       (flymake-simple-make-init)
 ;;     (my:flymake-simple-generic-init cmd opts)))
-;; ;;; fortran90 or 95
-;; ;(defun my:flymake-gfortran-init ()
-;; ;    (my:flymake-simple-make-or-generic-init
-;; ;        "gfortran" '("-O2" "-Wall" "-Wextra" "-fsyntax-only")))
-;; ;(defun my:flymake-g95-init ()
-;; ;    (my:flymake-simple-make-or-generic-init
-;; ;        "g95" '("-O2" "-Wall" "-Wextra" "-fsyntax-only")))
+;;; fortran90 or 95
+;(defun my:flymake-gfortran-init ()
+;    (my:flymake-simple-make-or-generic-init
+;        "gfortran" '("-O2" "-Wall" "-Wextra" "-fsyntax-only")))
+;(defun my:flymake-g95-init ()
+;    (my:flymake-simple-make-or-generic-init
+;        "g95" '("-O2" "-Wall" "-Wextra" "-fsyntax-only")))
 
-;; ;; minimal, non-make g95 setup
+;; minimal, non-make g95 setup
 ;; (defun flymake-g95-init ()
 ;;     (let* ((temp-file (flymake-init-create-temp-buffer-copy
 ;; 		            'flymake-create-temp-inplace))
@@ -369,7 +372,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default))))
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
+ '(quack-programs (quote ("mzscheme" "gauche" "bigloo" "csi" "csi -hygienic" "gosh" "gracket" "gsi" "gsi ~~/syntax-case.scm -" "guile" "kawa" "mit-scheme" "racket" "racket -il typed/racket" "rs" "scheme" "scheme48" "scsh" "sisc" "stklos" "sxi"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -408,11 +412,138 @@
 
 (require 'inertial-scroll)
 (setq inertias-global-minor-mode-map
-(inertias-define-keymap
-'(
-("<next>"  . inertias-up)
-("<prior>" . inertias-down)
-("C-v"     . inertias-up)
-("M-v"     . inertias-down)
-) inertias-prefix-key))
+      (inertias-define-keymap
+       '(
+	 ("<next>"  . inertias-up)
+	 ("<prior>" . inertias-down)
+	 ("C-v"     . inertias-up)
+	 ("M-v"     . inertias-down)
+	 ) inertias-prefix-key))
 (inertias-global-minor-mode 1)
+
+; Scheme (gosh)
+(require 'quack)
+
+(setq process-coding-system-alist
+      (cons '("gosh" utf-8 . utf-8) process-coding-system-alist))
+(setq scheme-program-name "/usr/bin/gosh -i")
+
+; scheme indentation
+(put 'and-let* 'scheme-indent-function 1)
+(put 'begin0 'scheme-indent-function 0)
+(put 'call-with-client-socket 'scheme-indent-function 1)
+(put 'call-with-input-conversion 'scheme-indent-function 1)
+(put 'call-with-input-file 'scheme-indent-function 1)
+(put 'call-with-input-process 'scheme-indent-function 1)
+(put 'call-with-input-string 'scheme-indent-function 1)
+(put 'call-with-iterator 'scheme-indent-function 1)
+(put 'call-with-output-conversion 'scheme-indent-function 1)
+(put 'call-with-output-file 'scheme-indent-function 1)
+(put 'call-with-output-string 'scheme-indent-function 0)
+(put 'call-with-temporary-file 'scheme-indent-function 1)
+(put 'call-with-values 'scheme-indent-function 1)
+(put 'dolist 'scheme-indent-function 1)
+(put 'dotimes 'scheme-indent-function 1)
+(put 'if-match 'scheme-indent-function 2)
+(put 'let*-values 'scheme-indent-function 1)
+(put 'let-args 'scheme-indent-function 2)
+(put 'let-keywords* 'scheme-indent-function 2)
+(put 'let-match 'scheme-indent-function 2)
+(put 'let-optionals* 'scheme-indent-function 2)
+(put 'let-syntax 'scheme-indent-function 1)
+(put 'let-values 'scheme-indent-function 1)
+(put 'let/cc 'scheme-indent-function 1)
+(put 'let1 'scheme-indent-function 2)
+(put 'letrec-syntax 'scheme-indent-function 1)
+(put 'make 'scheme-indent-function 1)
+(put 'match 'scheme-indent-function 1)
+(put 'match-lambda 'scheme-indent-function 1)
+(put 'match-let 'scheme-indent-fucntion 1)
+(put 'match-let* 'scheme-indent-fucntion 1)
+(put 'match-letrec 'scheme-indent-fucntion 1)
+(put 'match-let1 'scheme-indent-function 2)
+(put 'match-define 'scheme-indent-fucntion 1)
+(put 'multiple-value-bind 'scheme-indent-function 2)
+(put 'parameterize 'scheme-indent-function 1)
+(put 'parse-options 'scheme-indent-function 1)
+(put 'receive 'scheme-indent-function 2)
+(put 'rxmatch-case 'scheme-indent-function 1)
+(put 'rxmatch-cond 'scheme-indent-function 0)
+(put 'rxmatch-if  'scheme-indent-function 2)
+(put 'rxmatch-let 'scheme-indent-function 2)
+(put 'syntax-rules 'scheme-indent-function 1)
+(put 'unless 'scheme-indent-function 1)
+(put 'until 'scheme-indent-function 1)
+(put 'when 'scheme-indent-function 1)
+(put 'while 'scheme-indent-function 1)
+(put 'with-builder 'scheme-indent-function 1)
+(put 'with-error-handler 'scheme-indent-function 0)
+(put 'with-error-to-port 'scheme-indent-function 1)
+(put 'with-input-conversion 'scheme-indent-function 1)
+(put 'with-input-from-port 'scheme-indent-function 1)
+(put 'with-input-from-process 'scheme-indent-function 1)
+(put 'with-input-from-string 'scheme-indent-function 1)
+(put 'with-iterator 'scheme-indent-function 1)
+(put 'with-module 'scheme-indent-function 1)
+(put 'with-output-conversion 'scheme-indent-function 1)
+(put 'with-output-to-port 'scheme-indent-function 1)
+(put 'with-output-to-process 'scheme-indent-function 1)
+(put 'with-output-to-string 'scheme-indent-function 1)
+(put 'with-port-locking 'scheme-indent-function 1)
+(put 'with-string-io 'scheme-indent-function 1)
+(put 'with-time-counter 'scheme-indent-function 1)
+(put 'with-signal-handlers 'scheme-indent-function 1)
+
+; tumblsocks
+(require 'tumblesocks)
+
+; helm
+(require 'helm-config)
+(helm-mode 1)
+(define-key helm-read-file-map (kbd "<tab>") 'helm-execute-persistent-action)
+
+(when (require 'helm-config nil t)
+  (helm-mode 1)
+
+  (define-key global-map (kbd "M-x")     'helm-M-x)
+  (define-key global-map (kbd "C-x C-f") 'helm-find-files)
+  (define-key global-map (kbd "C-x C-r") 'helm-recentf)
+  (define-key global-map (kbd "M-y")     'helm-show-kill-ring)
+  (define-key global-map (kbd "C-c i")   'helm-imenu)
+  (define-key global-map (kbd "C-x b")   'helm-buffers-list)
+
+  (define-key helm-map (kbd "C-h") 'delete-backward-char)
+  (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+  (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+  (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+
+  ;; Disable helm in some functions
+  (add-to-list 'helm-completing-read-handlers-alist '(find-alternate-file . nil))
+
+  ;; Emulate `kill-line' in helm minibuffer
+  (setq helm-delete-minibuffer-contents-from-point t)
+  (defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)
+    "Emulate `kill-line' in helm minibuffer"
+    (kill-new (buffer-substring (point) (field-end))))
+
+  (defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
+    "Execute command only if CANDIDATE exists"
+    (when (file-exists-p candidate)
+      ad-do-it))
+
+  (defadvice helm-ff-transform-fname-for-completion (around my-transform activate)
+    "Transform the pattern to reflect my intention"
+    (let* ((pattern (ad-get-arg 0))
+	   (input-pattern (file-name-nondirectory pattern))
+	   (dirname (file-name-directory pattern)))
+      (setq input-pattern (replace-regexp-in-string "\\." "\\\\." input-pattern))
+      (setq ad-return-value
+	    (concat dirname
+		    (if (string-match "^\\^" input-pattern)
+			;; '^' is a pattern for basename
+			;; and not required because the directory name is prepended
+			(substring input-pattern 1)
+		      (concat ".*" input-pattern)))))))
+(shell-command-on-region (point-min) (point-max)
+    (concat "grep -n ^ " buffer-file-name)
+    (get-buffer-create "*tmp-linum*") nil t)

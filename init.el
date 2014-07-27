6cf1fa3dee95852c69a8a9b2a2f816154e79afa5
Junpei Anzai <junpei.anzai@gmail.com>
2014-07-28 08:37:14 +0900

    disable line-number-mode

--- init.el
+++ init.el
@@ -96,14 +96,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;; '(ansi-color-names-vector
-   ;; ["black" "red" "green" "yellow" "blue" "magenta" "cyan" "yellow"])
- ;; '(background-color nil)
- ;; '(background-mode dark)
- ;; '(cursor-color nil)
- ;; '(custom-safe-themes
- ;;   (quote
- ;;    ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
+;;  '(ansi-color-names-vector
+;;    ["black" "red" "green" "yellow" "blue" "magenta" "cyan" "yellow"])
+ '(background-color nil)
+ '(background-mode dark)
+ '(cursor-color nil)
+ '(custom-safe-themes
+   (quote
+    ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
  '(foreground-color nil)
  '(quack-programs
    (quote
@@ -138,7 +138,7 @@
 (global-hl-line-mode)
 
 (column-number-mode t)
-(line-number-mode t)
+;; (line-number-mode t)
 
 (require 'saveplace)
 (setq-default save-place t)
@@ -573,4 +573,26 @@
 
 (require 'smartparens-config)
 (smartparens-global-mode t)
+
+;; yasnippet
+;; http://www.gfd-dennou.org/member/uwabami/cc-env/emacs/yasnippet_config.html
+;; (require 'yasnippet nil 'noerror)
+;; (setq yas-snippet-dirs
+;;       (list "~/.emacs.d/share/snippets"))
+;; (yas-global-mode 1)
+;; ;; 単語展開キーバインド (ver8.0から明記しないと機能しない)
+;; (custom-set-variables '(yas-trigger-key (kbd "C-<tab>")))
+;; (setq yas-trigger-key (kbd "C-<tab>"))
+;; ;; 既存スニペットを挿入する
+;; (define-key yas-minor-mode-map (kbd "C-c C-x i i") 'yas-insert-snippet)
+;; ;; 新規スニペットを作成するバッファを用意する
+;; (define-key yas-minor-mode-map (kbd "C-c C-x i n") 'yas-new-snippet)
+;; ;; 既存スニペットを閲覧・編集する
+;; (define-key yas-minor-mode-map (kbd "C-c C-x i v") 'yas-visit-snippet-file)
+
+;; yasnippet.el----------------------------------
+(require 'yasnippet)
+(setq yas-snippet-dirs
+      '("~/.emacs.d/mySnippets"))
+(yas-global-mode 1)
 
